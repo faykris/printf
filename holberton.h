fae9a69e637015be0b104cc0a1f89b05d9bba57c
Adriana Echeverri <2903@holbertonschool.com>
2021-03-15 16:08:28 +0000

    Description corrected of structure

--- holberton.h
+++ holberton.h
@@ -46,7 +46,7 @@ void int_to_str(char *sbuffer, int number);
 /**
  * struct cp_func - structure holds data type and pointer.
  * @esp: especial character (s, f, i, d, ...).
- * @ptr_func: pointer to function with two pointers and va_list as paramenter.
+ * @ptr_func: pointer to function with pair pointers and va_list as paramenter.
  *
  * Description: structure that contains in the first position the char especial
  * and in the second one the pointer that manipulates that type of char.
